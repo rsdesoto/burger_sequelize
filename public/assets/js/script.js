bb6ecf143ddc21e03c4ad3976e4609bafdef4935
Ry <ry.e.desoto@gmail.com>
2019-01-09 20:18:21 -0600

    cleaning up alert

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,8 @@
 $(".devour-form").on("submit", function(event) {
   event.preventDefault();
-  //   //   alert(this:input.val());
-  //   var elements = this.elements;
-  //   console.log(elements);
-  //   //   alert(this.burger_id);
-  //   console.log(elements[0].value);
-  //   console.log(elements.input);
+
   id = this.elements[0].value;
-  alert(id);
+
   //   alert($(this).id);
   $.post("burgers/update/" + id, {
     id: id
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(routes);
 //   console.log("App listening on PORT " + PORT);
 // });
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({}).then(() => {
   app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
   });
