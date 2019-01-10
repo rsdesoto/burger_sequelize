ce657a3f77d392338218527dee0503155c3aba23
Ry <ry.e.desoto@gmail.com>
2019-01-09 18:42:37 -0600

    initial start

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var db = require("./models");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+// listen on port 3000
+
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
+
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+  });
+});
